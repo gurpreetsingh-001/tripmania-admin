f6934d825197ca1f4a08491bee93f17c84b2e88b
gurpreetsingh-001 <133507363+gurpreetsingh-001@users.noreply.github.com>
2024-04-30 22:59:12 +0530

    Delete backend directory

--- backend/routes/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {createBooking} = require('../controller/booking.controller');
-
-const bookingRoutes = express.Router();
-
-bookingRoutes.post('/create', createBooking);
-
-module.exports = bookingRoutes;
--- backend/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler=(err,req,res,next)=>{
-    console.log(err.message);
-    res.status(404).json({
-        message:err.message
-    })
-}
-
-
-module.exports=errorHandler;
